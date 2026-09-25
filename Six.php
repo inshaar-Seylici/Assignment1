70f4e59a3d74bd5116c81f89d6db7e562d77022c
Abdihakiim Inshaar <seylici157@gmail.com>
2026-09-25 08:45:43 +0300

    Adding File six

---
+++ Six.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+$num1 = 8;
+$num2 = 12;
+
+$lcm = $num1;
+
+while ($lcm % $num2 != 0) {
+    $lcm = $lcm + $num1;
+}
+
+echo "LCM is: " . $lcm;
+
+?>
+</body>
+</html>
+</body>
+</html>
